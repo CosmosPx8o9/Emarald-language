905de0b8130e0d9119adf439578673ca2553ae76
cosmosPx8o9 <daniil8642kill@gmail.com>
2026-07-30 16:18:14 +0500

    Vector modification: memcpy is used instead of void**.

--- common/Vector.c
+++ common/Vector.c
@@ -1,83 +1,96 @@
 #include "Vector.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
-char v_empty(vector* init) {
+bool v_empty(vector* init) {
 	if (!init) return 0;
-	return init->top == init->bottom;
+	return init->top <= 0;
 }
 
-int v_size(vector* init) {
+size_t v_top(vector* init) {
 	if (!init) return 25;
 	return init->top;
 }
 
-int v_init(vector** target, int botm, int size) {
-	if (!target) return 25;
+int v_init(vector* init, size_t size_element) {
+	if (!init) return 25;
+	init->ceiling = 8;
+	init->size    = size_element;
+	init->top     = 0;
 
-	vector* init = malloc(sizeof(vector));
-	if (!init) return 26;
-
-	init->pointers = malloc(size * sizeof(void*));
-	if (!init->pointers) { free(init); return 26;}
-
-	init->ceiling = size;
-	init->bottom  = botm;
-	init->top     = botm;
-
-	*target = init;
+	init->bytes = malloc(8 * size_element);
+	if (!init->bytes) return 26;
 	return 0;
 }
 
-int v_free(vector** init) {
-	if (!init || !(*init)) return 25;
-	if ((*init)->pointers) free((*init)->pointers);
-	free(*init);
+int v_free(vector* init) {
+	if (!init||!init->bytes) return 25;
+	free(init->bytes);
+	return 0;
+}
+
+vector* v_from(size_t size_element) {
+	vector* init = malloc(sizeof(vector));
+	if (!init||v_init(init, size_element)) return NULL;
+	return init;
+}
+
+int v_destroy(vector** init) {
+	if (!init||!(*init)) return 26;
+
+	if ((*init)->bytes) free((*init)->bytes);
+	free((*init));
 	*init = NULL;
 	return 0;
 }
 
+int v_reserve(vector* init, int s_new) {
+	if (!init||!init->bytes) return 25;
+	if (s_new <= init->ceiling) return 30;
+	unsigned char* temp = realloc(init->bytes, s_new * init->size);
+	if (!temp) return 26;
+	init->bytes = temp;
+	init->ceiling  = s_new;
+	return 0;
+}
+
 int v_push(vector* init, void* ptr) {
-	if (!init) return 25;
+	if (!init||!init->bytes||!ptr) return 25;
 
-	if (init->top + 1 > init->ceiling) {
-		void** temp;
-		printf("Realloc inded %d\n", init->top);
+	if (init->top >= init->ceiling) {
 		int s_new = init->ceiling * 2;
-		temp = realloc(init->pointers, s_new *sizeof(void*));
-		if (!temp) return 26;
-
-		init->pointers = temp;
-		init->ceiling = s_new;
+		if (v_reserve(init, s_new)) return 26;
 	}
-
-	printf("Write to %d\n", init->top);
-	init->pointers[init->top] = ptr;
+	void* offest = init->bytes + init->top * init->size;
+	memcpy(offest, ptr, init->size);
 	init->top++;
 	return 0;
 }
 
-int v_back(vector* init, void** resulf) {
+int v_back(vector* init, void* resulf) {
 	if (!init || !resulf) return 25;
 	if (v_empty(init)) return 30;
 
-	*resulf = init->pointers[init->top - 1];
+	void* offest = init->bytes + (init->top - 1) * init->size;
+	memcpy(resulf, offest, init->size);
 	return 0;
 }
 
-int v_pop(vector* init, void** resulf) {
+int v_pop(vector* init, void* resulf) {
 	if (!init || !resulf) return 25;
 	if (v_empty(init)) return 30;
 
 	init->top--;
-	*resulf = init->pointers[init->top];
+	void* offest = init->bytes + init->top * init->size;
+	memcpy(resulf, offest, init->size);
 	return 0;
 }
 
-int v_get(vector* init, int index, void** resulf) {
+int v_get(vector* init, int index, void* resulf) {
 	if (!init || !resulf) return 25;
-	if (index > init->bottom && index < init->top) {
-		*resulf = init->pointers[index];
+	if (index >= 0 && index < init->top) {
+		void* offest = init->bytes + init->top *init->size;
+		memcpy(resulf, offest, init->size);
 		return 0;
 	}
 	return 25;
--- common/Vector.h
+++ common/Vector.h
@@ -1,22 +1,29 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+#include <stdbool.h>
+#include <stddef.h>
 
 typedef struct {
-	void** pointers;
-	int bottom;
-	int ceiling;
-	int top;
+	unsigned char* bytes;
+	size_t size;
+	size_t ceiling;
+	size_t top;
 } vector;
 
-int v_init(vector**, int, int);
-int v_free(vector**);
+int v_init(vector*, size_t);
+int v_free(vector*);
+
+vector* v_from(size_t);
+int v_destroy(vector**);
+
+int v_reserve(vector*, int);
 
 int v_push(vector*, void*);
-int v_pop(vector*, void**);
-int v_back(vector*, void**);
-int v_get(vector*, int, void**);
+int v_pop(vector*, void*);
+int v_back(vector*, void*);
+int v_get(vector*, int, void*);
 
-int v_size(vector*);
-char v_empty(vector*);
+size_t v_top(vector*);
+bool v_empty(vector*);
 
 #endif
